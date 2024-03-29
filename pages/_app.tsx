8be63bac5b8554ea75205f35921998964b3e256d
alvinindra <alvin@antikode.com>
2024-03-29 22:59:34 +0700

    feat: setup appshell

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import '@mantine/core/styles.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, AppShell, Burger, Group, Skeleton, Title } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import { theme } from '../theme';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
+  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
+
   return (
     <MantineProvider theme={theme}>
       <Head>
@@ -15,7 +19,34 @@ export default function App({ Component, pageProps }: AppProps) {
         />
         <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
-      <Component {...pageProps} />
+      <AppShell
+        header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: 'sm',
+          collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+        }}
+        padding="md"
+      >
+        <AppShell.Header>
+          <Group h="100%" px="md">
+            <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
+            <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
+            <Title order={2}>Sistem Peminjaman Gedung</Title>
+          </Group>
+        </AppShell.Header>
+        <AppShell.Navbar p="md">
+          Navbar
+          {Array(15)
+            .fill(0)
+            .map((_, index) => (
+              <Skeleton key={index} h={28} mt="sm" animate={false} />
+            ))}
+        </AppShell.Navbar>
+        <AppShell.Main>
+          <Component {...pageProps} />
+        </AppShell.Main>
+      </AppShell>
     </MantineProvider>
   );
 }
